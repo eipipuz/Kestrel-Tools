517525a5fbc39ec5e5a4b41ac8fb325cb74d278b
Guillermo Alcantara <guillermo.alcantara@gmail.com>
2021-01-17 18:05:27 -0800

    Add Sorter

---
+++ src/main/kotlin/com/eipipuz/Sorter.kt
@@ -0,0 +1,55 @@
+package com.eipipuz
+
+
+object Sorter {
+    private fun <T : Comparable<T>> findPivot(list: List<T>): T {
+        val start = list.first()
+        val end = list.last()
+        val middleIndex: Int = list.size / 2
+        val middle = list[middleIndex]
+
+        return if (start <= end) {
+            if (start <= middle) {
+                if (middle <= end) middle else end
+            } else {
+                start
+            }
+        } else {
+            if (end <= middle) {
+                if (middle <= start) middle else start
+            } else {
+                end
+            }
+        }
+    }
+
+    fun <T : Comparable<T>> quickSort(collection: Collection<T>): List<T> {
+        val input = collection.toList()
+        if (collection.size <= 1) return input
+
+        val pivot = findPivot(input)
+
+        val lessThanList = mutableListOf<T>()
+        val equalThanList = mutableListOf<T>()
+        val moreThanList = mutableListOf<T>()
+
+        input.forEach {
+            when (it.compareTo(pivot)) {
+                -1 -> lessThanList.add(it)
+                0 -> equalThanList.add(it)
+                1 -> moreThanList.add(it)
+            }
+        }
+
+        val lessThanSorted = quickSort(lessThanList)
+        val moreThanSorted = quickSort(moreThanList)
+
+        return lessThanSorted + equalThanList + moreThanSorted
+    }
+
+    fun <T> mergeSort(collection: Collection<T>): List<T> = TODO()
+
+    fun <T> heapSort(collection: Collection<T>): List<T> = TODO()
+
+    fun <T> insertSort(collection: Collection<T>): List<T> = TODO()
+}
---
+++ src/test/kotlin/com/eipipuz/SorterTest.kt
@@ -0,0 +1,35 @@
+package com.eipipuz
+
+import org.junit.Test
+import kotlin.test.Ignore
+import kotlin.test.assertEquals
+
+
+class SorterTest {
+    companion object {
+        val list = listOf(2, 7, 8, 2, 1, 0)
+    }
+
+    @Test
+    fun testQuickSort() {
+        assertEquals(listOf(0, 1, 2, 2, 7, 8), Sorter.quickSort(list))
+    }
+
+    @Test
+    @Ignore
+    fun testHeapSort() {
+        assertEquals(listOf(0, 1, 2, 2, 7, 8), Sorter.heapSort(list))
+    }
+
+    @Test
+    @Ignore
+    fun testInsertSort() {
+        assertEquals(listOf(0, 1, 2, 2, 7, 8), Sorter.insertSort(list))
+    }
+
+    @Test
+    @Ignore
+    fun testMergeSort() {
+        assertEquals(listOf(0, 1, 2, 2, 7, 8), Sorter.mergeSort(list))
+    }
+}
