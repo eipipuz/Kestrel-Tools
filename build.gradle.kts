7fb292ae176c3670fa02c16a592bf7eeab9cafe8
Guillermo Alcantara <guillermo.alcantara@gmail.com>
2021-01-24 23:13:05 -0800

    Move towards gradle

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+val kotlinVersion = "1.4.21"
+
+plugins {
+    java
+    kotlin("jvm") version ("1.4.21")
+    `maven-publish`
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+    testImplementation("junit:junit:4.13.1")
+    testImplementation(kotlin("test-junit"))
+}
+
+group = "com.eipipuz"
+version = "0.1"
+description = "com.eipipuz kestrel-tools"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
